1827dd331662cf2fa8a1ba208f4d6b1abd564cb0
Myke Wyllian Dal'Agnol <mykewd@gmail.com>
2017-05-23 13:48:07 -0300

    Estrutura básica para iniciar o projeto

---
+++ src/projeto_1/Projeto_1.java
@@ -0,0 +1,16 @@
+package projeto_1;
+
+/**
+ *
+ * @autores Myke Wyllian Dal'Agnol e 
+ */
+public class Projeto_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo\nTeste");
+    }
+    
+}
