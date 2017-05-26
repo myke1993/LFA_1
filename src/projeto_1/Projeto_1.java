4b4aa64db4e08cebfd2f7af9f20d8b189de294bc
Myke Wyllian Dal'Agnol <mykewd@gmail.com>
2017-05-26 15:21:04 -0300

    Inclusão do artigo

--- src/projeto_1/Projeto_1.java
+++ src/projeto_1/Projeto_1.java
@@ -10,7 +10,7 @@ public class Projeto_1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Olá Mundo\nTeste");
+        System.out.println("Olá Mundo\nTeste2");
     }
     
 }
